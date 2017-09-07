5af63a1665e6ac5f5df97ba710df97c55f2529b3
drNymbus <30292340+drNymbus@users.noreply.github.com>
2017-09-07 18:48:08 +0200

    Change headers file
    risk_view.h and risk_initialize.h are merging into risk_view_initialize.h

--- Risk/risk_initializer.h
+++
@@ -1,3 +0,0 @@
-country_t* create_countries(char* path);
-void set_connnections(country_t* countries[], uint nb_countries);
-continent_t* create_continents(countries_t* countries[], uint nb_countries, char* path);
